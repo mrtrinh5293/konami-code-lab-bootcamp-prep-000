a2661fa770cabb47d263947834b2caeae7afc622
Duc Dang <mrtrinh5293@gmail.com>
2018-04-30 21:31:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
+let index = 0;
+function init(e) {
   // Write your JavaScript code inside the init() function
-
+const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
